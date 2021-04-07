4b3ad51f0addc133dd9b06477b5f806329b922b9
jsHunter <470759230@qq.com>
2021-04-07 17:47:42 +0800

    frame init

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
 <template>
+  <div id="header">
+    <h1>My Drf-Vue Blog</h1>
+    <hr />
+  </div>
+
   <div id="articles" v-for="article in info.results" :key="article.url">
     <div>
       s
@@ -12,6 +17,10 @@
     </div>
 
     <div>{{ formatted_time(article.created) }}</div>
+  </div>
+
+  <div id="footer">
+    <p>guoqy.com</p>
   </div>
 </template>
 
@@ -33,12 +42,31 @@ export default {
     formatted_time(iso_date_string) {
       const date = new Date(iso_date_string);
       return date.toLocaleDateString();
-    }
+    },
   },
 };
 </script>
 
 <style lang="less">
+#app {
+  font-family: Georgia, Arial, sans-serif;
+  margin-left: 40px;
+  margin-right: 40px;
+}
+#header {
+  text-align: center;
+  margin-top: 20px;
+}
+#footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  height: 50px;
+  width: 100%;
+  background-color: whitesmoke;
+  text-align: center;
+  font-weight: bold;
+}
 #articles {
   padding: 10px;
 }
@@ -48,5 +76,14 @@ export default {
   color: black;
   text-decoration: none;
   padding: 5px 0 5px 0;
+}
+.tag {
+  padding: 2px 5px 2px 5px;
+  margin: 5px 5px 5px 0;
+  font-family: Georgia, Arial, sans-serif;
+  font-size: small;
+  background-color: #4e4e4e;
+  color: whitesmoke;
+  border-radius: 5px;
 }
 </style>
